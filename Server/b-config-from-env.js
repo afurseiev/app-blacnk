95013a2a7e619a8b3857713871ed65282763ae75
Andrii Furseiev <afurseiev@amcbridge.com>
2021-11-08 14:02:09 +0200

    Updated configuration.

--- Server/b-config-from-env.js
+++ Server/b-config-from-env.js
@@ -4,8 +4,6 @@ const {isValidUrl}      = require('./z-utilities');
 const {isValidHttpUrl}  = require('./z-utilities');
 const {isValidString}   = require('./z-utilities');
 
-//import {isValidUrl, isValidHttpUrl, isValidString} from './z-utilities';
-
 //Configuration data should be extracted from environment successfully, otherwise  app will not run.
 //See the server console for detailed description in case of start failure
 
@@ -22,84 +20,7 @@ const redisPort = process.env.REDIS_PORT;
 const sessionSecret = process.env.SESSION_SECRET;
 const webhookCallbackRootUrl = process.env.WEBHOOK_CALLBACK_ROOT_URL;
 
-
-
-// ===========================================Service functions definitions ============================================
-
-
-/**
- * Checks if the given string is a URL. A string considered a URL if it can be parsed
- * as a URL (based on the WHATWG definition).
- * If `protocols` is provided, this will be taken into account in the validation.
- * 
- * For example:
- * 
- * `isValidUrl('https://example.com', [ 'http:', 'https:' ])` would evaluate to `true`
- * 
- * `isValidUrl('http://sub.example.com', [ 'redis:' ])` would evaluate to `false`
- * 
- * `isValidUrl('example.com')` would evaluate to `false`
- * 
- * @param {string} stringToTest The string to check for validity.
- * @param {string|string[]} protocols The protocol(s) to include in the validity
- *      check. May be excluded, in which case it will not be considered in the check.
- * 
- * @returns {boolean} `true` if the given string is a valid URL, and has one of the
- *      given protocols (if provided); or `false` otherwise.
- */
-/*
- const isValidUrl = function(stringToTest, protocols) {
-    try {
-        const url = new URL(stringToTest);
-        if (!protocols) {
-            return true;
-        }
-        if (typeof protocols === 'string' || protocols instanceof String) {
-            protocols = [ protocols ];
-        }
-        return !protocols || protocols.includes(url.protocol);
-    } catch {
-        return false;
-    }
-}
-
-/**
- * Checks if the given string is an HTTP or HTTPS URL. A string is considered if it can
- * be parsed as a URL (based on the WHATWG definition).
- * 
- * For example:
- * 
- * `isValidHttpUrl('http://example.com')` would evaluate to `true`
- * 
- * `isValidHttpUrl('ftp://user:pass@ftp.example.com/public/doc.txt)` would evaluate
- * to `false`
- * 
- * `isValidHttpUrl('example.com')` would evaluate to `false`
- * 
- * @param {string} stringToTest The string to check for validity.
- */
-/*
- const isValidHttpUrl = function(stringToTest) {
-    return isValidUrl(stringToTest, [ 'http:', 'https:' ]);
-};
-
-/**
- * Checks if the given string has content, i.e. is not null and does not contain solely  whitespace characters.
- * @param {string} stringToTest The string to check for validity.
- */
-/*
- const isValidString = function(stringToTest) {
-    if (!stringToTest) return false;
-    if (!(stringToTest.trim())) return false;
-    return true;
-}
-*/
-
-
-
-
 // 2 - Checking the entire configuration and only throw one error (if invalid).
-
 const errors = [];
 
 if (port && !isValidString(port))                           errors.push('PORT must have content');
@@ -118,7 +39,6 @@ if (!isValidHttpUrl(webhookCallbackRootUrl))                errors.push('WEBHOOK
 if (errors.length !== 0) {
     throw new Error('Invalid configuration: ' + errors.join(', '));
 }
-
 
 module.exports = {
     //- mandatory                        
