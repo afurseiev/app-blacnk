c76ad7b727599dd34e5384eb1410ed7a9a3c323c
Andrii Furseiev <afurseiev@amcbridge.com>
2021-11-08 15:22:39 +0200

    Updated configuration.

--- Server/c-server-module.js
+++ Server/c-server-module.js
@@ -1,19 +1,19 @@
 
 // external dependencies 
-const path = require('path');
-const uuid = require('uuid');
-
-const express = require('express');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-
-const RedisStore = require('connect-redis')(session);
-const passport = require('passport');
-const OnshapeStrategy = require('passport-onshape');
+const path              = require('path');
+const uuid              = require('uuid');
+const express           = require('express');
+const session           = require('express-session');
+const bodyParser        = require('body-parser');
+const RedisStore        = require('connect-redis')(session);
+const passport          = require('passport');
+const OnshapeStrategy   = require('passport-onshape');
 
 // internal dependencies
-const config = require('./b-config-from-env');
-const redisClient = require('./d-redis-client');
+const config            = require('./b-config-from-env');
+const redisClient       = require('./d-redis-client');
+
+let RedirectAddress = `https://oauth.onshape.com/oauth/authorize?response_type=code&client_id=${config.oauthClientId}`;
 
 
 const app = express();
@@ -74,13 +74,31 @@ app.use('/oauthRedirect', passport.authenticate('onshape', { failureRedirect: '/
 });
 
 app.get('/grantDenied', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Client', 'html', 'grantDenied.html'));
+    res.sendFile(path.join(__dirname, '../Client', 'html', 'grantDenied.html'));
 })
 
 // providing Front end web page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../Client', 'html', 'index.html'));
 });
+
+// checking the Open Autorization 
+app.get ('/doesAutorized',  (req, res) => {
+    if (req.user == undefined) {
+        console.log ("Redirecting to autorization");
+        console.log (RedirectAddress);
+        res.status(401).send(RedirectAddress);
+    }
+    else if  (req.user.accessToken == undefined) {
+        console.log ("Redirecting to autorization");
+        console.log (RedirectAddress);
+        res.status(401).send(RedirectAddress);
+    }
+    else {
+        res.status(200).send();
+    }
+});
+
 
 // Redirecting API queries
 // app.use('/api', require('./api')); 
