84234c24a2adaa335e7b026948e7edca00305cee
Andrii Furseiev <afurseiev@amcbridge.com>
2021-11-10 16:56:45 +0200

    Updated configuration.

--- public/javascripts/blanc.js
+++ public/javascripts/blanc.js
@@ -1,11 +1,41 @@
 
 var element = document.getElementById('getElementsResult');
 
-function getElements ()
-{
- let response = async  () => {
+async function getElements ()
+  {
+    let response = await fetch(`/api/elements${window.location.search}`, { headers: { 'Accept': 'application/json' } });
+    if (response.ok)
+    {
+      let responseText =  await response.text();
+      return responseText;
+    }
+    else
+    {
+      return "Ошибка HTTP: " + response.status;
+    }
+  }
+  
+  /*
+  (async () => {
+    let response = await fetch(`/api/elements${window.location.search}`, { headers: { 'Accept': 'application/json' } });
+    //let status = await response.status;
+    let responseText = await response.text();
+    return responseText;
+  })
+      console.log (responseText);
+      return responseText;
+    }
+    else 
+    {
+      return "Ошибка HTTP: " + response.status;
+    }
+  }
+
+
+/* let response = async  () => {
      await fetch(`/api/elements${window.location.search}`, { headers: { 'Accept': 'application/json' } })
   };
+
   if (response.ok) { 
     let responseText = response.text;
     responseText = JSON.stringify(responseText);
@@ -16,7 +46,7 @@ function getElements ()
   }
 
 };
-
+*/
 
 element.textContent = getElements();
 
