70b325d9f36506d2d93abeb0fe6af7c99b4be30e
Andrii Furseiev <afurseiev@amcbridge.com>
2021-11-10 10:51:57 +0200

    Updated configuration.

--- public/javascripts/blanc.js
+++ public/javascripts/blanc.js
@@ -7,17 +7,20 @@ const fetch = require('node-fetch');
 var element = document.getElementById('getElementsResult');
 //element.textContent = getDocumentElements();
 
- 
+
+async function getElements ()
+{
 let response = await fetch('/api/elements');
 if (response.ok) { 
     let responseText = await response.text();
     console.log (responseText);
-    element.textContent = responseText;
+    return responseText;
   } else {
     alert("Ошибка HTTP: " + response.status);
-    element.textContent = response.status;
+    return response.status;
   }
+};
       
-
+element = getElements();
 
 
