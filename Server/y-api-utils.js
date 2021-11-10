acab1610ebbadb5facbf1669e0eebd8e992b7f07
Andrii Furseiev <afurseiev@amcbridge.com>
2021-11-10 14:04:54 +0200

    Updated configuration.

--- Server/y-api-utils.js
+++ Server/y-api-utils.js
@@ -1,6 +1,7 @@
 
 const { onshapeApiUrl } = require('./b-config-from-env');
 const fetch             = require('node-fetch');
+const refresh           = require('passport-oauth2-refresh');
 
 module.exports = 
 {
@@ -13,15 +14,36 @@ module.exports =
     forwardRequestToOnshape: async (apiPath, req, res) => {
         const normalizedUrl = apiPath.indexOf(onshapeApiUrl) === 0 ? apiPath : `${onshapeApiUrl}/${apiPath}`;
         const onshapeResponse = await fetch(normalizedUrl, { headers: { Authorization: `Bearer ${req.user.accessToken}` }});
+        const status = await onshapeResponse.status;
         if (onshapeResponse.ok)
         {  
-            console.log ("OK status = " + onshapeResponse.status);
             const data = await onshapeResponse.text();
             const contentType = onshapeResponse.headers.get('Content-Type');
             res.status(onshapeResponse.status).contentType(contentType).send(data);
         }
         else
         {
+            if (status == '401')
+            {
+                let OldRrefreshToken = req.user.refreshToken;
+        
+                refresh.requestNewAccessToken('onshape', OldRrefreshToken, function(err, accessToken, refreshToken) {
+                req.user.accessToken = accessToken;
+                req.user.refreshToken = refreshToken;
+           
+                if (accessToken)
+                {
+                    console.log("Restarting forwarder...");
+                    forwardRequestToOnshape (req, res);
+                }
+                else
+                {
+                    console.log(JSON.stringify(err));
+                    console.log (RedirectAddress);
+                    res.status(401).send(RedirectAddress);
+                }       
+                });
+            }
             console.log ("FAIL status = " + onshapeResponse.status);
             console.log ("FAIL text = " + onshapeResponse.statusText);
             res.status(onshapeResponse.status).send(onshapeResponse.statusText);  
