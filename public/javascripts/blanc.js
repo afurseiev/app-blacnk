05c755f624d09bf4f6e4d5f72dc01fd8b8f3edcb
Andrii Furseiev <afurseiev@amcbridge.com>
2021-11-11 12:02:00 +0200

    Updated configuration.

--- public/javascripts/blanc.js
+++ public/javascripts/blanc.js
@@ -8,38 +8,62 @@ var element = document.getElementById('getElementsResult');
 function tableCreate(dataArray) {
   var body = document.getElementsByTagName('body')[0]; //get the first body-type object available on web page
   
-  //let tableRowCount = dataArray.length + 1; //calculate row number of the future table
+  let tableRowCount = dataArray.length; //calculate row number of the future table
   
-  var tbl = document.createElement('table'); //create table
-  tbl.style.width = '60%';                    //define it's style
-  tbl.setAttribute('border', '1');
+  var elementTable = document.createElement('table'); //create table
+  elementTable.style.width = '60%';                    //define it's style
+  elementTable.setAttribute('border', '1');
 
   //create table header
   let tableHeader = document.createElement('thead');
+  let headerRow   = document.createElement('tr');
+  let headNumber  = document.createElement('th');
+  let headType    = document.createElement('th');
+  let headName    = document.createElement('th');
 
-  let headerRow = document.createElement('tr');
-
-  let headNumber = document.createElement('th');
-  let headType = document.createElement('th');
-  let headName = document.createElement('th');
-
- // headNumber.appendChild (document.createTextNode("Number"));
+  headNumber.appendChild (document.createTextNode("Number"));
   headType.appendChild (document.createTextNode("Type"));
   headName.appendChild (document.createTextNode("Name"));
 
-  //headerRow.appendChild (headNumber);
-  headerRow.appendChild (document.createElement('th').appendChild(document.createTextNode("Number")));
+  headerRow.appendChild (headNumber);
   headerRow.appendChild (headType);
   headerRow.appendChild (headName);
 
-  
-
-
   tableHeader.appendChild(headerRow);
 
+  elementTable.appendChild(tableHeader);
 
 
-  //var tbdy = document.createElement('tbody');
+  //create table body
+  let tableBody = document.createElement('tbody');
+
+  for (var i = 0; i < tableRowCount; i++) 
+  {
+    let tableRow = document.createElement('tr');
+    for (let J = 0; J < 3; J++) 
+    {
+      let rowCell = document.createElement('td');
+      let content = undefined;
+      switch (J)
+      {
+        case "0":
+          content = i+1;
+          break;
+        case "1":
+          content = dataArray[i].type;
+          break;
+        case "2":
+          content = dataArray[i].name; 
+          break;   
+      }  
+      rowCell.appendChild (document.createTextNode(content));
+      tableRow.appendChild(rowCell);
+    }
+    tableBody.appendChild(tableRow);
+  }
+
+  elementTable.appendChild(tableBody);
+  
   /*for (var i = 0; i < tableRowCount; i++) {
     var tr = document.createElement('tr');
     for (var j = 0; j < 3; j++) {
@@ -56,8 +80,8 @@ function tableCreate(dataArray) {
     tbdy.appendChild(tr);
   }
   */
-  tbl.appendChild(tableHeader);
-  body.appendChild(tbl)
+ // tbl.appendChild(tableHeader);
+  body.appendChild(elementTable);
 }
 
 
