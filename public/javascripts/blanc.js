44b2d8969edd17cd8d062a9d7399d102b4c0ef29
Andrii Furseiev <afurseiev@amcbridge.com>
2021-11-11 09:06:22 +0200

    Updated configuration.

--- public/javascripts/blanc.js
+++ public/javascripts/blanc.js
@@ -15,39 +15,31 @@ async function getElements ()
     }
   }
   
-  /*
-  (async () => {
-    let response = await fetch(`/api/elements${window.location.search}`, { headers: { 'Accept': 'application/json' } });
-    //let status = await response.status;
-    let responseText = await response.text();
-    return responseText;
-  })
-      console.log (responseText);
-      return responseText;
-    }
-    else 
-    {
-      return "Ошибка HTTP: " + response.status;
-    }
-  }
-
-
-/* let response = async  () => {
-     await fetch(`/api/elements${window.location.search}`, { headers: { 'Accept': 'application/json' } })
-  };
-
-  if (response.ok) { 
-    let responseText = response.text;
-    responseText = JSON.stringify(responseText);
-    console.log (responseText);
-    return responseText;
-  } else {
-    return "Ошибка HTTP: " + response.status;
-  }
-
-};
-*/
 
 getElements();
 
 
+function tableCreate() {
+  var body = document.getElementsByTagName('body')[0];
+  var tbl = document.createElement('table');
+  tbl.style.width = '100%';
+  tbl.setAttribute('border', '1');
+  var tbdy = document.createElement('tbody');
+  for (var i = 0; i < 3; i++) {
+    var tr = document.createElement('tr');
+    for (var j = 0; j < 2; j++) {
+      if (i == 2 && j == 1) {
+        break
+      } else {
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode('\u0020'))
+        i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+        tr.appendChild(td)
+      }
+    }
+    tbdy.appendChild(tr);
+  }
+  tbl.appendChild(tbdy);
+  body.appendChild(tbl)
+}
+tableCreate();
